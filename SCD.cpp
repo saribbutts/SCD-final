c7ce1b57f0c259628df6a47ce7ca2abf597885a7
saribbutts <157739769+saribbutts@users.noreply.github.com>
2024-01-29 12:53:57 +0500

    Update SCD.cpp

--- SCD.cpp
+++ SCD.cpp
@@ -3,9 +3,13 @@
 
 class Room {
 	void  SearchModule();
+        //performed function
 	void HotelDetailsModule();
+        //performed function
 	void BookingModule();
+       //performed function
 	void Availability();
+       //performed function
 };
 
 int main()
